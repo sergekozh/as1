277cead40fc70b9b89e3e7468e648602b0d9e361
Sergey Kozhevnikov <sergekozh@outlook.com>
2017-11-07 15:26:45 -0800

    comment1
    comment1-1

---
+++ Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Threading;
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        
+        Response.Write("<br>");
+        Response.Write(Server.MachineName.ToString());
+        Response.Write("<br>");
+        Response.Write(DateTime.Now.ToString());
+        //Response.Write(b/a);
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int a = 0;
+        int b = 1;
+
+        var t = b / a;
+    }
+}
